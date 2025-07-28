349561fe647ce331ca618201a345d44ccf03021d
Tabare Sanchez <tabaregabriel@gmail.com>
2025-07-28 11:49:53 -0300

    se crea funcionalidad para el boton de reiniciar

--- logica programacion 2/app.js
+++ logica programacion 2/app.js
@@ -1,4 +1,9 @@
 let numeroSecreto = generarNumeroSecreto();
+let intentos = 0;
+
+condicionesIniciales();
+
+console.log("numero secreto: " + numeroSecreto);
 
 function asignarTextoElemento(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
@@ -8,21 +13,31 @@ function asignarTextoElemento(elemento, texto) {
 
 function verificarIntento() {
     let numeroDeUsuario = parseInt(document.getElementById("valorUsuario").value);
+    intentos++;
 
-    if(numeroDeUsuario === numeroSecreto) {
-        asignarTextoElemento("p", "Acertaste el número secreto");
-    }
-    else if (numeroDeUsuario > numeroSecreto) {
-        asignarTextoElemento("p", "El número secreto es menor");
-    }
-    else if (numeroDeUsuario < numeroSecreto) {
-        asignarTextoElemento("p", "El número secreto es mayor");
+    if (numeroDeUsuario === numeroSecreto) {
+        asignarTextoElemento("p", "Acertaste el número secreto en " + intentos + " " + (intentos == 1 ? " intento" : " intentos"));
+        document.getElementById("reiniciar").removeAttribute("disabled");
     }
     else {
-        asignarTextoElemento("p", "Por favor, introduce un número válido");
+        if (numeroDeUsuario > numeroSecreto) {
+            asignarTextoElemento("p", "El número secreto es menor");
+        }
+        else if (numeroDeUsuario < numeroSecreto) {
+            asignarTextoElemento("p", "El número secreto es mayor");
+        }
+        else {
+            asignarTextoElemento("p", "Por favor, introduce un número válido");
+        }
+        limpiarCaja();
     }
 
     return;
+}
+
+function limpiarCaja() {
+    document.querySelector("#valorUsuario").value = "";
+    document.querySelector("#valorUsuario").focus();
 }
 
 function generarNumeroSecreto() {
@@ -30,5 +45,16 @@ function generarNumeroSecreto() {
     return numeroSecreto;
 }
 
-asignarTextoElemento("h1", "Juego del número secreto");
-asignarTextoElemento("p", "Indica un número del 1 al 10");
+function condicionesIniciales() {
+    asignarTextoElemento("h1", "Juego del número secreto");
+    asignarTextoElemento("p", "Indica un número del 1 al 10");
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 0;
+}
+
+function reiniciarJuego() {
+    limpiarCaja();
+    condicionesIniciales();
+    document.querySelector("#reiniciar").setAttribute("disabled", "true");
+}
+
