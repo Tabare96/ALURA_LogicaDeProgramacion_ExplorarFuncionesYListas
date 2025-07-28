12d3b70427cbb69a9d4f02e6750088f9c0e09247
Tabare Sanchez <tabaregabriel@gmail.com>
2025-07-28 12:30:17 -0300

    realizo segunda tanda de ejercicios

---
+++ logica programacion 2/Desafio_Funciones.js
@@ -0,0 +1,48 @@
+//1
+// function calcularIMC(peso, altura) {
+//     return peso / (altura * altura);
+// }
+
+//2
+// function factorial(n) {
+//     if (n === 0 || n === 1) {
+//         return 1;
+//     }
+//     let resultado = 1;
+//     for (let i = 2; i <= n; i++) {
+//         resultado *= i;
+//     }
+//     return resultado;
+// }
+
+//3
+// function dolaresAReales(numero) {
+//     return numero * 4.8;
+// }
+
+//4
+// function calcularAreaYPerimetro(base, altura) {
+//     let area = (base * altura) / 2;
+//     let perimetro = base + altura + Math.sqrt(base * base + altura * altura);
+//     return ("Área: " + area + ", Perímetro: " + perimetro);
+// }
+// console.log(calcularAreaYPerimetro(3, 4));
+
+//5
+// function calcularAreaYPerimetroEnBaseAlRadio(radio) {
+//     let pi = 3.14;
+//     let area = pi * radio * radio;
+//     let perimetro = 2 * pi * radio;
+//     return ("Área: " + area + ", Perímetro: " + perimetro);
+// }
+// console.log(calcularAreaYPerimetroEnBaseAlRadio(15));
+
+//6
+// function mostrarTablaDeMultiplicar(numero) {
+//     let tabla = "tabla de multiplicar del " + numero + ":\n";
+//     for (let i = 1; i <= 10; i++) {
+//         tabla += numero + " x " + i + " = " + (numero * i) + "\n";
+//     }
+//     return tabla;
+// }
+// console.log(mostrarTablaDeMultiplicar(5));
